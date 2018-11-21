47d6a29283e0a7aa48b37086365e92114aa8aa0a
Ali Rezaei <ali.rezaei09@gmail.com>
2018-11-21 11:45:22 +0330

    Convert Static method in Dagger Module class to Kotlin

---
+++ app/src/main/java/com/sample/android/tmdb/ui/detail/DetailModule.kt
@@ -0,0 +1,27 @@
+package com.sample.android.tmdb.ui.detail
+
+import com.sample.android.tmdb.di.ActivityScoped
+import com.sample.android.tmdb.di.FragmentScoped
+import com.sample.android.tmdb.ui.detail.DetailActivity.Companion.EXTRA_MOVIE
+import com.sample.android.tmdb.vo.Movie
+import dagger.Module
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class DetailModule {
+
+    @FragmentScoped
+    @ContributesAndroidInjector
+    internal abstract fun detailFragment(): DetailFragment
+
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        @ActivityScoped
+        internal fun provideMovie(activity: DetailActivity): Movie =
+                activity.intent.extras.getParcelable(EXTRA_MOVIE)
+    }
+
+}
