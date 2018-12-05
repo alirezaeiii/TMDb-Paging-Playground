a5feb76c19f94a7fcea2b7c9f7a4590a3ac93f06
Ali Rezaei <ali.rezaei09@gmail.com>
2018-12-05 17:45:36 +0330

    Remove extra class

--- app/src/main/java/com/sample/android/tmdb/MovieAppGlideModule.kt
+++
@@ -1,8 +0,0 @@
-package com.sample.android.tmdb
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class MovieAppGlideModule : AppGlideModule() {
-}
