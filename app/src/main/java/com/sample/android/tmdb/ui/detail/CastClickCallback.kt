f9cc817c028fc5a3057ae5d6c223f8100320144d
Ali Rezaei <ali.rezaei09@gmail.com>
2019-08-05 19:50:02 +0430

    Remove unnecessary imports

--- app/src/main/java/com/sample/android/tmdb/ui/detail/CastClickCallback.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/detail/CastClickCallback.kt
@@ -1,8 +1,5 @@
 package com.sample.android.tmdb.ui.detail
 
-import android.widget.ImageView
-import android.widget.TextView
-
 interface CastClickCallback {
     fun onClick(personId: Int, personName: String, profilePath: String?)
 }
