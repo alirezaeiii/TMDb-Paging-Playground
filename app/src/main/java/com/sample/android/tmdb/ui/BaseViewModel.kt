758ea3ab91e6a6949483c6554bf476629c7827a6
Ali Rezaei <ali.rezaei09@gmail.com>
2019-09-26 18:18:01 +0330

    Add a comment

--- app/src/main/java/com/sample/android/tmdb/ui/BaseViewModel.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/BaseViewModel.kt
@@ -7,6 +7,12 @@ open class BaseViewModel : ViewModel() {
 
     protected val compositeDisposable = CompositeDisposable()
 
+    /**
+     * Called when the ViewModel is dismantled.
+     * At this point, we want to cancel all disposables;
+     * otherwise we end up with processes that have nowhere to return to
+     * using memory and resources.
+     */
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.clear()
