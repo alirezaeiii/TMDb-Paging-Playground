0e299c3da222b230f85a3a2a1b61bf4239c89e50
Ali Rezaei <ali.rezaei09@gmail.com>
2020-01-19 16:11:56 +0330

    Encapsulation

--- app/src/main/java/com/sample/android/tmdb/repository/bypage/ItemDataSourceFactory.kt
+++ app/src/main/java/com/sample/android/tmdb/repository/bypage/ItemDataSourceFactory.kt
@@ -1,18 +1,21 @@
 package com.sample.android.tmdb.repository.bypage
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.DataSource
 import com.sample.android.tmdb.domain.TmdbItem
 
 abstract class ItemDataSourceFactory<T : TmdbItem, E> : DataSource.Factory<Int, T>() {
 
-    val sourceLiveData = MutableLiveData<PageKeyedItemDataSource<T, E>>()
+    private val _sourceLiveData = MutableLiveData<PageKeyedItemDataSource<T, E>>()
+    val sourceLiveData: LiveData<PageKeyedItemDataSource<T, E>>
+        get() = _sourceLiveData
 
     protected abstract fun getDataSource(): PageKeyedItemDataSource<T, E>
 
     override fun create(): DataSource<Int, T> {
         val source = getDataSource()
-        sourceLiveData.postValue(source)
+        _sourceLiveData.postValue(source)
         return source
     }
 }
--- app/src/main/java/com/sample/android/tmdb/repository/bypage/PageKeyedItemDataSource.kt
+++ app/src/main/java/com/sample/android/tmdb/repository/bypage/PageKeyedItemDataSource.kt
@@ -1,5 +1,6 @@
 package com.sample.android.tmdb.repository.bypage
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.PageKeyedDataSource
 import com.sample.android.tmdb.domain.TmdbItem
@@ -21,9 +22,13 @@ abstract class PageKeyedItemDataSource<T : TmdbItem, E>(
      * There is no sync on the state because paging will always call loadInitial first then wait
      * for it to return some success value before calling loadAfter.
      */
-    val networkState = MutableLiveData<NetworkState>()
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState: LiveData<NetworkState>
+        get() = _networkState
 
-    val initialLoad = MutableLiveData<NetworkState>()
+    private val _initialLoad = MutableLiveData<NetworkState>()
+    val initialLoad: LiveData<NetworkState>
+        get() = _initialLoad
 
     fun retryAllFailed() {
         val prevRetry = retry
@@ -39,51 +44,45 @@ abstract class PageKeyedItemDataSource<T : TmdbItem, E>(
 
     protected abstract fun fetchItems(page: Int): Call<E>
 
-    override fun loadBefore(
-            params: LoadParams<Int>,
-            callback: LoadCallback<Int, T>) {
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, T>) {
         // ignored, since we only ever append to our initial load
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, T>) {
 
-        networkState.postValue(NetworkState.LOADING)
-        fetchItems(params.key).enqueue(
-                object : retrofit2.Callback<E> {
-                    override fun onFailure(call: Call<E>, t: Throwable) {
-                        // keep a lambda for future retry
-                        retry = {
-                            loadAfter(params, callback)
-                        }
-                        networkState.postValue(NetworkState.error(t.message ?: "unknown err"))
-                    }
+        _networkState.postValue(NetworkState.LOADING)
+        fetchItems(params.key).enqueue(object : retrofit2.Callback<E> {
 
-                    override fun onResponse(
-                            call: Call<E>,
-                            response: Response<E>) {
-                        if (response.isSuccessful) {
-                            // clear retry since last request succeeded
-                            retry = null
-                            callback.onResult(getItems(response), params.key + 1)
-                            networkState.postValue(NetworkState.LOADED)
-                        } else {
-                            retry = {
-                                loadAfter(params, callback)
-                            }
-                            networkState.postValue(
-                                    NetworkState.error("error code: ${response.code()} " +
-                                            response.message()))
-                        }
-                    }
+            override fun onFailure(call: Call<E>, t: Throwable) {
+                // keep a lambda for future retry
+                retry = {
+                    loadAfter(params, callback)
                 }
+                _networkState.postValue(NetworkState.error(t.message ?: "unknown err"))
+            }
+
+            override fun onResponse(call: Call<E>, response: Response<E>) {
+                if (response.isSuccessful) {
+                    // clear retry since last request succeeded
+                    retry = null
+                    callback.onResult(getItems(response), params.key + 1)
+                    _networkState.postValue(NetworkState.LOADED)
+                } else {
+                    retry = {
+                        loadAfter(params, callback)
+                    }
+                    _networkState.postValue(
+                            NetworkState.error("error code: ${response.code()} " +
+                                    response.message()))
+                }
+            }
+        }
         )
     }
 
-    override fun loadInitial(
-            params: LoadInitialParams<Int>,
-            callback: LoadInitialCallback<Int, T>) {
-        networkState.postValue(NetworkState.LOADING)
-        initialLoad.postValue(NetworkState.LOADING)
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, T>) {
+        _networkState.postValue(NetworkState.LOADING)
+        _initialLoad.postValue(NetworkState.LOADING)
 
         // The network request might be handled in a different thread so make sure Espresso knows
         // that the app is busy until the response is handled.
@@ -94,16 +93,16 @@ abstract class PageKeyedItemDataSource<T : TmdbItem, E>(
             val response = fetchItems(1).execute()
             if (response.isSuccessful) {
                 retry = null
-                networkState.postValue(NetworkState.LOADED)
-                initialLoad.postValue(NetworkState.LOADED)
+                _networkState.postValue(NetworkState.LOADED)
+                _initialLoad.postValue(NetworkState.LOADED)
                 callback.onResult(getItems(response), null, 2)
             } else {
                 retry = {
                     loadInitial(params, callback)
                 }
                 val error = NetworkState.error("error code: ${response.code()} " + response.message())
-                networkState.postValue(error)
-                initialLoad.postValue(error)
+                _networkState.postValue(error)
+                _initialLoad.postValue(error)
             }
             if (!EspressoIdlingResource.getIdlingResource().isIdleNow) {
                 EspressoIdlingResource.decrement() // Set app as idle.
@@ -113,8 +112,8 @@ abstract class PageKeyedItemDataSource<T : TmdbItem, E>(
                 loadInitial(params, callback)
             }
             val error = NetworkState.error(ioException.message ?: "unknown error")
-            networkState.postValue(error)
-            initialLoad.postValue(error)
+            _networkState.postValue(error)
+            _initialLoad.postValue(error)
         }
     }
 }
