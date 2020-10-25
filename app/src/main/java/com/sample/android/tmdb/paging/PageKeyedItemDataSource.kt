a237fb3f3b244fd9e9ed1200a96b329d712bef9e
Ali Rezaei <ali.rezaei09@gmail.com>
2020-10-25 16:22:53 +0330

    More specific error messge

--- app/src/main/java/com/sample/android/tmdb/paging/PageKeyedItemDataSource.kt
+++ app/src/main/java/com/sample/android/tmdb/paging/PageKeyedItemDataSource.kt
@@ -8,6 +8,7 @@ import com.sample.android.tmdb.R
 import com.sample.android.tmdb.domain.TmdbItem
 import com.sample.android.tmdb.domain.ItemWrapper
 import com.sample.android.tmdb.util.EspressoIdlingResource
+import com.sample.android.tmdb.util.isNetworkAvailable
 import retrofit2.Call
 import retrofit2.Response
 import java.io.IOException
@@ -50,69 +51,85 @@ abstract class PageKeyedItemDataSource<T : TmdbItem>(
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, T>) {
-
         _networkState.postValue(NetworkState.LOADING)
-        fetchItems(params.key).enqueue(object : retrofit2.Callback<ItemWrapper<T>> {
+        if (context.isNetworkAvailable()) {
+            fetchItems(params.key).enqueue(object : retrofit2.Callback<ItemWrapper<T>> {
 
-            override fun onFailure(call: Call<ItemWrapper<T>>, t: Throwable) {
-                // keep a lambda for future retry
-                retry = {
-                    loadAfter(params, callback)
-                }
-                _networkState.postValue(NetworkState.error(context.getString(R.string.failed_loading_msg)))
-            }
-
-            override fun onResponse(call: Call<ItemWrapper<T>>, response: Response<ItemWrapper<T>>) {
-                if (response.isSuccessful) {
-                    // clear retry since last request succeeded
-                    retry = null
-                    callback.onResult(response.body()?.items ?: emptyList(), params.key + 1)
-                    _networkState.postValue(NetworkState.LOADED)
-                } else {
+                override fun onFailure(call: Call<ItemWrapper<T>>, t: Throwable) {
+                    // keep a lambda for future retry
                     retry = {
                         loadAfter(params, callback)
                     }
                     _networkState.postValue(NetworkState.error(context.getString(R.string.failed_loading_msg)))
                 }
+
+                override fun onResponse(call: Call<ItemWrapper<T>>, response: Response<ItemWrapper<T>>) {
+                    if (response.isSuccessful) {
+                        // clear retry since last request succeeded
+                        retry = null
+                        callback.onResult(response.body()?.items ?: emptyList(), params.key + 1)
+                        _networkState.postValue(NetworkState.LOADED)
+                    } else {
+                        retry = {
+                            loadAfter(params, callback)
+                        }
+                        _networkState.postValue(NetworkState.error(context.getString(R.string.failed_loading_msg)))
+                    }
+                }
+            })
+        } else {
+            retry = {
+                loadAfter(params, callback)
             }
-        })
+            _networkState.postValue(NetworkState.error(context.getString(R.string.failed_network_msg)))
+        }
     }
 
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, T>) {
         _networkState.postValue(NetworkState.LOADING)
         _initialLoad.postValue(NetworkState.LOADING)
 
-        // The network request might be handled in a different thread so make sure Espresso knows
-        // that the app is busy until the response is handled.
-        EspressoIdlingResource.increment() // App is busy until further notice
+        if (context.isNetworkAvailable()) {
+            // The network request might be handled in a different thread so make sure Espresso knows
+            // that the app is busy until the response is handled.
+            EspressoIdlingResource.increment() // App is busy until further notice
 
-        // triggered by a refresh, we better execute sync
-        try {
-            val response = fetchItems(1).execute()
-            if (response.isSuccessful) {
-                retry = null
-                _networkState.postValue(NetworkState.LOADED)
-                _initialLoad.postValue(NetworkState.LOADED)
-                callback.onResult(response.body()?.items ?: emptyList(), null, 2)
-            } else {
+            // triggered by a refresh, we better execute sync
+            try {
+                val response = fetchItems(1).execute()
+                if (response.isSuccessful) {
+                    retry = null
+                    _networkState.postValue(NetworkState.LOADED)
+                    _initialLoad.postValue(NetworkState.LOADED)
+                    callback.onResult(response.body()?.items ?: emptyList(), null, 2)
+                } else {
+                    retry = {
+                        loadInitial(params, callback)
+                    }
+                    val error = NetworkState.error(context.getString(R.string.failed_loading_msg))
+                    _networkState.postValue(error)
+                    _initialLoad.postValue(error)
+                }
+            } catch (ioException: IOException) {
                 retry = {
                     loadInitial(params, callback)
                 }
                 val error = NetworkState.error(context.getString(R.string.failed_loading_msg))
                 _networkState.postValue(error)
                 _initialLoad.postValue(error)
+
+            } finally {
+                if (!EspressoIdlingResource.getIdlingResource().isIdleNow) {
+                    EspressoIdlingResource.decrement() // Set app as idle.
+                }
             }
-        } catch (ioException: IOException) {
+        } else {
             retry = {
                 loadInitial(params, callback)
             }
-            val error = NetworkState.error(context.getString(R.string.failed_loading_msg))
+            val error = NetworkState.error(context.getString(R.string.failed_network_msg))
             _networkState.postValue(error)
             _initialLoad.postValue(error)
-        } finally {
-            if (!EspressoIdlingResource.getIdlingResource().isIdleNow) {
-                EspressoIdlingResource.decrement() // Set app as idle.
-            }
         }
     }
 }
--- app/src/main/java/com/sample/android/tmdb/util/ContextExt.kt
+++ app/src/main/java/com/sample/android/tmdb/util/ContextExt.kt
@@ -1,6 +1,31 @@
 package com.sample.android.tmdb.util
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.view.LayoutInflater
 
 val Context.layoutInflater: LayoutInflater get() = LayoutInflater.from(this)
+
+@Suppress("DEPRECATION")
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    } else {
+        val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+        return nwInfo.isConnected
+    }
+}
