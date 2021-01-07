bd92c54296eb59a718825c008712821dca3f6e76
Ali Rezaei <ali.rezaei09@gmail.com>
2021-01-07 15:29:05 +0330

    Simplify

--- app/src/main/java/com/sample/android/tmdb/network/OkHttpProvider.kt
+++ app/src/main/java/com/sample/android/tmdb/network/OkHttpProvider.kt
@@ -1,12 +1,9 @@
 package com.sample.android.tmdb.network
 
 import com.sample.android.tmdb.BuildConfig
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
-import java.io.IOException
 
 object OkHttpProvider {
 
@@ -18,25 +15,18 @@ object OkHttpProvider {
         }
         logger.level = HttpLoggingInterceptor.Level.BASIC
 
-        instance = OkHttpClient.Builder()
-                .addInterceptor(RequestInterceptor())
+        instance = OkHttpClient.Builder().addInterceptor { chain ->
+                    val original = chain.request()
+                    val originalHttpUrl = original.url()
+
+                    val url = originalHttpUrl.newBuilder()
+                            .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+                            .build()
+
+                    val request = original.newBuilder().url(url).build()
+                    chain.proceed(request)
+                }
                 .addInterceptor(logger)
                 .build()
-    }
-
-    private class RequestInterceptor : Interceptor {
-
-        @Throws(IOException::class)
-        override fun intercept(chain: Interceptor.Chain): Response {
-            val original = chain.request()
-            val originalHttpUrl = original.url()
-
-            val url = originalHttpUrl.newBuilder()
-                    .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
-                    .build()
-
-            val request = original.newBuilder().url(url).build()
-            return chain.proceed(request)
-        }
     }
 }
