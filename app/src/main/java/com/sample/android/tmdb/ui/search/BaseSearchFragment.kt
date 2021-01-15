8caf250e7f3fe8cf9e9e751e5a74be9295cddcaa
Ali Rezaei <ali.rezaei09@gmail.com>
2021-01-15 22:16:05 +0330

    Modification

--- app/src/main/java/com/sample/android/tmdb/ui/search/BaseSearchFragment.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/search/BaseSearchFragment.kt
@@ -2,7 +2,7 @@ package com.sample.android.tmdb.ui.search
 
 import androidx.lifecycle.Observer
 import com.sample.android.tmdb.domain.TmdbItem
-import com.sample.android.tmdb.paging.Status
+import com.sample.android.tmdb.paging.Status.RUNNING
 import com.sample.android.tmdb.ui.BaseFragment
 import com.sample.android.tmdb.ui.NavType
 import com.sample.android.tmdb.ui.TmdbAdapter
@@ -11,9 +11,9 @@ import kotlinx.android.synthetic.main.fragment_main.*
 
 abstract class BaseSearchFragment<T : TmdbItem> : BaseFragment<T>() {
 
-    override val navType: NavType by lazy { (activity as SearchActivity).navType }
-
     private val searchViewModel by lazy { viewModel as BaseSearchViewModel }
+
+    override val navType: NavType by lazy { (activity as SearchActivity).navType }
 
     override fun refresh() {
         super.refresh()
@@ -30,7 +30,7 @@ abstract class BaseSearchFragment<T : TmdbItem> : BaseFragment<T>() {
 
     fun observeNetworkState() {
         searchViewModel.refreshState.observe(this, Observer {
-            recyclerView.toVisibility(it.status != Status.RUNNING)
+            recyclerView.toVisibility(it.status != RUNNING)
         })
     }
 }
