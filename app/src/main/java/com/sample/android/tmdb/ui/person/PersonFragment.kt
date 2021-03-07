e55efcfa0a4c8c2885574fdff2f9f796eda83862
Ali Rezaei <ali.rezaei09@gmail.com>
2021-03-07 21:54:31 +0330

    Revert

--- app/src/main/java/com/sample/android/tmdb/ui/person/PersonFragment.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/person/PersonFragment.kt
@@ -15,11 +15,15 @@ import kotlinx.android.synthetic.main.fragment_person.view.*
 import kotlinx.android.synthetic.main.person_header.view.*
 import javax.inject.Inject
 
-class PersonFragment @Inject constructor
-(private val personWrapper: PersonWrapper) : DaggerFragment() {
+class PersonFragment @Inject
+constructor() // Required empty public constructor
+    : DaggerFragment() {
 
     @Inject
     lateinit var factory: PersonViewModel.Factory
+
+    @Inject
+    lateinit var personWrapper: PersonWrapper
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
