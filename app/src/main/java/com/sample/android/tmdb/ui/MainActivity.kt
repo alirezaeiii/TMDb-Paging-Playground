0417b957a704cc77a6cd957a49ff2492df141a9a
Ali Rezaei <ali.rezaei09@gmail.com>
2021-06-07 02:55:40 +0430

    Using onSaveInstanceState for rotation in MainActivity

--- app/src/main/java/com/sample/android/tmdb/ui/MainActivity.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/MainActivity.kt
@@ -39,20 +39,18 @@ class MainActivity : DaggerAppCompatActivity() {
     lateinit var upcomingMoviesFragment: UpcomingMoviesFragment
 
     @Inject
-    lateinit var popularTVShowFragment: PopularTVShowFragment
+    lateinit var popularTvShowFragment: PopularTVShowFragment
 
     @Inject
-    lateinit var highRateTVShowFragment: HighRateTVShowFragment
+    lateinit var highRateTvShowFragment: HighRateTVShowFragment
 
     @Inject
-    lateinit var latestTVShowFragment: LatestTVShowFragment
+    lateinit var latestTvShowFragment: LatestTVShowFragment
 
     private lateinit var viewModel: MainViewModel
 
-    private lateinit var movieFragment: Fragment
-    private lateinit var tvShowFragment: Fragment
-    private var movieItemId: Int = R.id.action_movie_popular
-    private var tvShowItemId: Int = R.id.action_tvShow_popular
+    private lateinit var movieWrapper: FragmentWrapper
+    private lateinit var tvShowWrapper: FragmentWrapper
 
     val navType: NavType?
         get() = viewModel.currentType.value
@@ -63,28 +61,36 @@ class MainActivity : DaggerAppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        if (savedInstanceState == null) {
-            movieFragment = popularMoviesFragment
-            tvShowFragment = popularTVShowFragment
-            // Add popular movieFragment if this is first creation
-            addFragmentToActivity(movieFragment, R.id.fragment_container)
-            nav_view.setCheckedItem(R.id.action_movies)
-        }
-
-        setupNavigationView()
-        setupBottomNavigation()
-
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar,
-            R.string.open_nav_drawer, R.string.close_nav_drawer
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.headline.observe(this, {
             title = it
         })
+
+        if (savedInstanceState == null) {
+            FragmentWrapper(popularMoviesFragment, R.id.action_movie_popular).also { movieWrapper = it }
+            FragmentWrapper(popularTvShowFragment, R.id.action_tvShow_popular).also { tvShowWrapper = it }
+            // Add popular movieFragment if this is first creation
+            addFragmentToActivity(movieWrapper.fragment, R.id.fragment_container)
+            nav_view.setCheckedItem(R.id.action_movies)
+        } else {
+            val movieItemId = savedInstanceState.getInt(MOVIE_ITEM_ID_KEY)
+            val tvShowItemId = savedInstanceState.getInt(TV_SHOW_ITEM_ID_KEY)
+            FragmentWrapper(getFragment(movieItemId), movieItemId).also { movieWrapper = it }
+            FragmentWrapper(getFragment(tvShowItemId), tvShowItemId).also { tvShowWrapper = it }
+            when (navType) {
+                NavType.MOVIES -> inflateMenu(R.menu.bottom_navigation_menu_movie)
+                NavType.TV_SERIES -> inflateMenu(R.menu.bottom_navigation_menu_tv_show)
+            }
+        }
+
+        setupBottomNavigation()
+        setupNavigationView()
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+            R.string.open_nav_drawer, R.string.close_nav_drawer
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -121,6 +127,12 @@ class MainActivity : DaggerAppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(MOVIE_ITEM_ID_KEY, movieWrapper.itemId)
+        outState.putInt(TV_SHOW_ITEM_ID_KEY, tvShowWrapper.itemId)
+    }
+
     // ============================================================================================
     //  Private setup methods
     // ============================================================================================
@@ -132,14 +144,14 @@ class MainActivity : DaggerAppCompatActivity() {
                 R.id.action_movies -> {
                     viewModel.setType(R.string.menu_movies, NavType.MOVIES)
                     inflateMenu(R.menu.bottom_navigation_menu_movie)
-                    bottom_navigation.selectedItemId = movieItemId
-                    movieFragment
+                    bottom_navigation.selectedItemId = movieWrapper.itemId
+                    movieWrapper.fragment
                 }
                 R.id.action_tv_series -> {
                     viewModel.setType(R.string.menu_tv_series, NavType.TV_SERIES)
-                    inflateMenu(R.menu.bottom_navigation_menu_tvshow)
-                    bottom_navigation.selectedItemId = tvShowItemId
-                    tvShowFragment
+                    inflateMenu(R.menu.bottom_navigation_menu_tv_show)
+                    bottom_navigation.selectedItemId = tvShowWrapper.itemId
+                    tvShowWrapper.fragment
                 }
                 else -> throw RuntimeException("Unknown navType to replace fragment")
             }
@@ -151,21 +163,7 @@ class MainActivity : DaggerAppCompatActivity() {
     private fun setupBottomNavigation() {
         bottom_navigation.apply {
             setOnNavigationItemSelectedListener { item ->
-                val fragment = when (item.itemId) {
-                    R.id.action_movie_popular -> getMovieFragment(popularMoviesFragment)
-                    R.id.action_movie_highest_rate -> getMovieFragment(highRateMoviesFragment)
-                    R.id.action_movie_upcoming -> getMovieFragment(upcomingMoviesFragment)
-                    R.id.action_tvShow_popular -> getTvShowFragment(popularTVShowFragment)
-                    R.id.action_tvShow_highest_rate -> getTvShowFragment(highRateTVShowFragment)
-                    R.id.action_tvShow_latest -> getTvShowFragment(latestTVShowFragment)
-                    else -> throw RuntimeException("Unknown sortType to replace fragment")
-                }
-                when (navType) {
-                    NavType.MOVIES -> movieItemId = item.itemId
-                    NavType.TV_SERIES -> tvShowItemId = item.itemId
-                }
-
-                replaceFragmentInActivity(fragment, R.id.fragment_container)
+                replaceFragmentInActivity(getFragment(item.itemId), R.id.fragment_container)
                 true
             }
         }
@@ -176,13 +174,42 @@ class MainActivity : DaggerAppCompatActivity() {
         bottom_navigation.inflateMenu(menuId)
     }
 
-    private fun getMovieFragment(fragment: Fragment): Fragment {
-        movieFragment = fragment
+    private fun getFragment(itemId: Int): Fragment {
+        return when (itemId) {
+            R.id.action_tvShow_popular -> getTvShowFragment(popularTvShowFragment, itemId)
+            R.id.action_tvShow_highest_rate -> getTvShowFragment(highRateTvShowFragment, itemId)
+            R.id.action_tvShow_latest -> getTvShowFragment(latestTvShowFragment, itemId)
+            R.id.action_movie_popular -> getMovieFragment(popularMoviesFragment, itemId)
+            R.id.action_movie_highest_rate -> getMovieFragment(highRateMoviesFragment, itemId)
+            R.id.action_movie_upcoming -> getMovieFragment(upcomingMoviesFragment, itemId)
+            else -> throw RuntimeException("Unknown sortType to replace fragment")
+        }
+    }
+
+    private fun getMovieFragment(fragment: Fragment, itemId: Int): Fragment {
+        if(::movieWrapper.isInitialized) {
+            movieWrapper.setItem(fragment, itemId)
+        }
         return fragment
     }
 
-    private fun getTvShowFragment(fragment: Fragment): Fragment {
-        tvShowFragment = fragment
+    private fun getTvShowFragment(fragment: Fragment, itemId: Int): Fragment {
+        if(::tvShowWrapper.isInitialized) {
+            tvShowWrapper.setItem(fragment, itemId)
+        }
         return fragment
+    }
+
+    private class FragmentWrapper(var fragment: Fragment, var itemId: Int) {
+
+        fun setItem(fragment: Fragment, itemId: Int) {
+            this.fragment = fragment
+            this.itemId = itemId
+        }
+    }
+
+    companion object {
+        private const val MOVIE_ITEM_ID_KEY = "movie_id"
+        private const val TV_SHOW_ITEM_ID_KEY = "tvShow_id"
     }
 }
