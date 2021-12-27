3a1ce44a954cdf9ac338e0623c8dbc15b9cea7d1
Ali Rezaei <ali.rezaei09@gmail.com>
2021-12-27 19:44:08 +0330

    Cleanup

--- app/src/main/java/com/sample/android/tmdb/ui/paging/BaseFragment.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/paging/BaseFragment.kt
@@ -27,7 +27,7 @@ abstract class BaseFragment<T : TmdbItem> : DaggerFragment(), TmdbClickCallback<
 
     protected abstract val viewModel: BaseViewModel<T>
 
-    protected abstract val navType: NavType?
+    protected abstract val navType: NavType
 
     protected open fun refresh() {
         viewModel.refresh()
