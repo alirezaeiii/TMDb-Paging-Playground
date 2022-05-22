8a3bcf46c1a8a9966569c0c29e508bb3e8e4fdaa
Ali Rezaei <ali.rezaei09@gmail.com>
2022-05-22 17:09:25 +0430

    Catch ActivityNotFoundException

--- app/src/main/java/com/sample/android/tmdb/util/BindingUtils.kt
+++ app/src/main/java/com/sample/android/tmdb/util/BindingUtils.kt
@@ -1,5 +1,6 @@
 package com.sample.android.tmdb.util
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.databinding.BindingAdapter
 import android.graphics.Bitmap
@@ -9,6 +10,7 @@ import androidx.cardview.widget.CardView
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -21,21 +23,24 @@ import de.hdodenhof.circleimageview.CircleImageView
 @BindingAdapter("ImageUrl")
 fun bindImageItem(cardView: CardView, url: String?) {
     Glide.with(cardView.context)
-            .asBitmap()
-            .load(url)
-            .apply(RequestOptions().centerCrop())
-            .into(object : BitmapImageViewTarget(cardView.findViewById(R.id.item_poster)) {
-                override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap>?) {
-                    super.onResourceReady(bitmap, transition)
-                    Palette.from(bitmap).generate { palette ->
-                        val color = palette!!.getVibrantColor(
-                                ContextCompat.getColor(cardView.context,
-                                        R.color.black_translucent_60))
+        .asBitmap()
+        .load(url)
+        .apply(RequestOptions().centerCrop())
+        .into(object : BitmapImageViewTarget(cardView.findViewById(R.id.item_poster)) {
+            override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap>?) {
+                super.onResourceReady(bitmap, transition)
+                Palette.from(bitmap).generate { palette ->
+                    val color = palette!!.getVibrantColor(
+                        ContextCompat.getColor(
+                            cardView.context,
+                            R.color.black_translucent_60
+                        )
+                    )
 
-                        cardView.findViewById<View>(R.id.title_background).setBackgroundColor(color)
-                    }
+                    cardView.findViewById<View>(R.id.title_background).setBackgroundColor(color)
                 }
-            })
+            }
+        })
 }
 
 @BindingAdapter("imageUrl")
@@ -49,9 +54,9 @@ fun addItems(linearLayout: LinearLayout, trailers: List<Video>?) {
     val context = linearLayout.context
 
     val options = RequestOptions()
-            .placeholder(R.color.colorPrimary)
-            .centerCrop()
-            .override(150, 150)
+        .placeholder(R.color.colorPrimary)
+        .centerCrop()
+        .override(150, 150)
 
 
     trailers?.forEach { trailer ->
@@ -61,14 +66,19 @@ fun addItems(linearLayout: LinearLayout, trailers: List<Video>?) {
         thumbView.apply {
             setOnClickListener {
                 val playVideoIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Video.getUrl(trailer)))
-                context.startActivity(playVideoIntent)
+                try {
+                    context.startActivity(playVideoIntent)
+                } catch (ex: ActivityNotFoundException) {
+                    Toast.makeText(context, R.string.application_not_found, Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
         }
 
         Glide.with(context)
-                .load(Video.getThumbnailUrl(trailer))
-                .apply(options)
-                .into(thumbView)
+            .load(Video.getThumbnailUrl(trailer))
+            .apply(options)
+            .into(thumbView)
 
         linearLayout.addView(thumbContainer)
     }
@@ -77,22 +87,24 @@ fun addItems(linearLayout: LinearLayout, trailers: List<Video>?) {
 @BindingAdapter("profileUrl")
 fun bindProfileImage(imageView: ImageView, url: String?) {
     val options = RequestOptions()
-            .centerCrop()
-            .error(R.drawable.ic_error_outline_black_36dp)
+        .centerCrop()
+        .error(R.drawable.ic_error_outline_black_36dp)
 
     Glide.with(imageView.context)
-            .load(IMAGE_LOW_RES_BASE_URL.plus(url))
-            .apply(options)
-            .into(imageView)
+        .load(IMAGE_LOW_RES_BASE_URL.plus(url))
+        .apply(options)
+        .into(imageView)
 }
 
 @BindingAdapter("profileUrl")
 fun bindProfileImage(imageView: CircleImageView, url: String?) {
     Glide.with(imageView.context)
-            .load(IMAGE_LOW_RES_BASE_URL.plus(url))
-            .apply(RequestOptions()
-                    .error(R.drawable.ic_error_outline_white_96dp))
-            .into(imageView)
+        .load(IMAGE_LOW_RES_BASE_URL.plus(url))
+        .apply(
+            RequestOptions()
+                .error(R.drawable.ic_error_outline_white_96dp)
+        )
+        .into(imageView)
 }
 
 @BindingAdapter("goneIfNull")
