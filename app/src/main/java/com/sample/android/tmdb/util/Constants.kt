712f72e9f6b64c87bf41927391174a3557406da2
Ali Rezaei <ali.rezaei09@gmail.com>
2022-05-26 17:50:07 +0430

    Cleanup

--- app/src/main/java/com/sample/android/tmdb/util/Constants.kt
+++ app/src/main/java/com/sample/android/tmdb/util/Constants.kt
@@ -2,7 +2,5 @@ package com.sample.android.tmdb.util
 
 object Constants {
     const val EXTRA_TMDB_ITEM = "tmdbItem"
-    const val EXTRA_NAV_TYPE = "nav_type"
-    const val EXTRA_SORT_TYPE = "sort_type"
     const val PERSON_WRAPPER = "person"
 }
