6891aedebd9ae23266e31e7185fd6c597b2ab870
Ali <ali.rezaei09@gmail.com>
2022-09-12 22:59:00 +0430

    Separate model classes

--- app/src/main/java/com/sample/android/tmdb/domain/Video.kt
+++ app/src/main/java/com/sample/android/tmdb/domain/Video.kt
@@ -2,18 +2,6 @@ package com.sample.android.tmdb.domain
 
 import com.google.gson.annotations.SerializedName
 
-class Person(
-        @SerializedName("birthday")
-        val birthDay: String?,
-        @SerializedName("deathday")
-        val deathDay: String?,
-        val id: Int,
-        @SerializedName("also_known_as")
-        val alsoKnowAs: Array<String>,
-        val biography: String,
-        @SerializedName("place_of_birth")
-        val placeOfBirth: String?)
-
 class Video(
         val id: String,
         val name: String,
