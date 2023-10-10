fa134611a18468a62d7aa0fa123eefd06646de6f
Ali <ali.rezaei09@gmail.com>
2023-10-10 23:03:59 +0330

    Cleanup imports

--- app/src/main/java/com/sample/android/tmdb/di/RepositoryModule.kt
+++ app/src/main/java/com/sample/android/tmdb/di/RepositoryModule.kt
@@ -7,11 +7,10 @@ import com.sample.android.tmdb.data.repository.TVShowDetailRepositoryImpl
 import com.sample.android.tmdb.data.repository.TVShowFeedRepository
 import com.sample.android.tmdb.domain.model.Movie
 import com.sample.android.tmdb.domain.model.TVShow
+import com.sample.android.tmdb.domain.repository.BaseFeedRepository
 import com.sample.android.tmdb.domain.repository.MovieDetailRepository
 import com.sample.android.tmdb.domain.repository.PersonRepository
 import com.sample.android.tmdb.domain.repository.TVShowDetailRepository
-import com.sample.android.tmdb.domain.repository.BaseFeedRepository
-import com.sample.android.tmdb.repository.*
 import dagger.Binds
 import dagger.Module
 import javax.inject.Singleton
