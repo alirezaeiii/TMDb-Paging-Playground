142c344e1131b144a7f4b689f037218e346d17bf
slee_fisker <slee@fiskerinc.com>
2023-12-15 14:45:54 -0800

    Revert

--- app/src/main/java/com/sample/android/tmdb/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/sample/android/tmdb/ui/feed/FeedFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
-import com.sample.android.tmdb.domain.model.FeedWrapper
 import com.sample.android.tmdb.domain.model.TmdbItem
 import com.sample.android.tmdb.ui.base.BaseNavigationFragment
 import com.sample.android.tmdb.ui.common.Content
@@ -13,8 +11,6 @@ import com.sample.android.tmdb.ui.common.TmdbTheme
 import com.sample.android.tmdb.ui.common.composeView
 
 abstract class FeedFragment<T : TmdbItem> : BaseNavigationFragment<Nothing>() {
-
-    private var feeds: List<FeedWrapper<T>>? = null
 
     protected abstract val viewModel: FeedViewModel<T>
 
@@ -25,25 +21,15 @@ abstract class FeedFragment<T : TmdbItem> : BaseNavigationFragment<Nothing>() {
     ): View {
         return composeView {
             TmdbTheme {
-                if (feeds == null) {
-                    Content(viewModel) {
-                        feeds = it
-                        FeedCollectionScreen()
+                Content(viewModel) {
+                    FeedCollectionList(
+                        navType,
+                        it
+                    ) { tmdbItem ->
+                        startDetailActivity(tmdbItem)
                     }
-                } else {
-                    FeedCollectionScreen()
                 }
             }
-        }
-    }
-
-    @Composable
-    private fun FeedCollectionScreen() {
-        FeedCollectionList(
-            navType,
-            feeds!!
-        ) { tmdbItem ->
-            startDetailActivity(tmdbItem)
         }
     }
 }
